ac92ad7679448bde3f86a2be6614d22a80112f78
Arizel79 <arizel79@gmail.com>
2026-07-07 13:19:21 +0300

    replace Cell struct with bool

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,6 @@ use std::fmt;
 use std::thread;
 use std::time::Duration;
 
-#[derive(Debug, Clone)]
-struct Cell {
-    is_alive: bool,
-}
-
-impl Cell {
-    fn new(is_alive: bool) -> Cell {
-        Cell { is_alive }
-    }
-}
-
 const WEIGHT: u32 = 100;
 const HEIGHT: u32 = 30;
 
@@ -23,25 +12,25 @@ const WAIT_TIME: u64 = 100;
 const ALIVE_CELL: char = 'O';
 const DEAD_CELL: char = '.';
 
-fn display_cells(cells: &HashMap<(u32, u32), Cell>) {
+fn display_cells(cells: &HashMap<(u32, u32), bool>) {
     let mut output = String::with_capacity((WEIGHT * HEIGHT + HEIGHT) as usize);
     for y in 0..HEIGHT {
         for x in 0..WEIGHT {
             let cell = cells.get(&(x, y)).unwrap();
-            output.push(if cell.is_alive { ALIVE_CELL } else { DEAD_CELL });
+            output.push(if *cell { ALIVE_CELL } else { DEAD_CELL });
         }
         output.push('\n');
     }
     print!("{}", output);
 }
 
-fn update_cells(cells: HashMap<(u32, u32), Cell>) -> HashMap<(u32, u32), Cell> {
-    let mut new_cells: HashMap<(u32, u32), Cell> = HashMap::new();
+fn update_cells(cells: HashMap<(u32, u32), bool>) -> HashMap<(u32, u32), bool> {
+    let mut new_cells: HashMap<(u32, u32), bool> = HashMap::new();
 
     for y in 0..HEIGHT {
         for x in 0..WEIGHT {
-            let option_cell: Option<&Cell> = cells.get(&(x, y));
-            let cell: &Cell = option_cell.unwrap();
+            let option_cell: Option<&bool> = cells.get(&(x, y));
+            let cell: &bool = option_cell.unwrap();
 
             let left_x = (x + WEIGHT - 1) % WEIGHT;
             let right_x = (x + 1) % WEIGHT;
@@ -63,30 +52,30 @@ fn update_cells(cells: HashMap<(u32, u32), Cell>) -> HashMap<(u32, u32), Cell> {
 
             for i in &neighbors_variant {
                 if let Some(cell) = cells.get(&i) {
-                    if cell.is_alive {
+                    if *cell {
                         neighbors_number += 1;
                     }
                 }
             }
 
-            if cell.is_alive && (neighbors_number == 2 || neighbors_number == 3) {
-                new_cells.insert((x, y), Cell::new(true));
-            } else if !cell.is_alive && neighbors_number == 3 {
-                new_cells.insert((x, y), Cell::new(true));
+            if *cell && (neighbors_number == 2 || neighbors_number == 3) {
+                new_cells.insert((x, y), true);
+            } else if !*cell && neighbors_number == 3 {
+                new_cells.insert((x, y), true);
             } else {
-                new_cells.insert((x, y), Cell::new(false));
+                new_cells.insert((x, y), false);
             }
         }
     }
     new_cells
 }
 
-fn generate_cells() -> HashMap<(u32, u32), Cell> {
+fn generate_cells() -> HashMap<(u32, u32), bool> {
     let mut rng = rand::rng();
-    let mut cells: HashMap<(u32, u32), Cell> = HashMap::new();
+    let mut cells: HashMap<(u32, u32), bool> = HashMap::new();
     for x in 0..WEIGHT {
         for y in 0..HEIGHT {
-            cells.insert((x, y), Cell::new(rng.random_bool(0.5)));
+            cells.insert((x, y), rng.random_bool(0.5));
         }
     }
     cells
